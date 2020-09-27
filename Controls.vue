23fd73d8a18959423d46bac419656ec4ae357504
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-09-27 14:00:30 +0700

    + controls component

---
+++ Controls.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<button>
+			{{ post.likes }}
+		</button>
+		<Hashtag v-for="tag in post.hashtags" :key="tag" :hashtag="tag" @setHashtag="setHashtag" />
+	</div>
+</template>
+
+<script>
+import Hashtag from "./Hashtag.vue";
+export default {
+	props: {
+		post: {
+			type: Object,
+		},
+	},
+	components: {
+		Hashtag,
+	},
+	setup(props, ctx) {
+        const setHashtag = (tag) => {
+            console.log(tag);
+            ctx.emit('setHashtag', tag);
+        }
+
+        return {
+            setHashtag
+        }
+	},
+};
+</script>
+
+<style scoped>
+</style>
