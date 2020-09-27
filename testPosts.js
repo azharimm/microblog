af57d906368babab95210c711558af6444edcaa0
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-09-27 14:00:11 +0700

    + testposts data

---
+++ testPosts.js
@@ -0,0 +1,30 @@
+export const testPosts = [
+    {
+        id: 1,
+        title: 'Learning Vue 3',
+        content: 'I am learning Vue 3 with the composition API',
+        likes: 10,
+        hashtags: ['vue', 'javascript', 'composition api']
+    },
+    {
+        id: 2,
+        title: 'Learning Vuex',
+        content: 'Vuex is a state management solution for Vue App',
+        likes: 3,
+        hashtags: ['vuex', 'vue', 'flux']
+    },
+    {
+        id: 3,
+        title: 'Routing with Vue Router',
+        content: 'I am creating a complex frontend app using Vue Router',
+        likes: 12,
+        hashtags: ['vue', 'vue-router']
+    },
+    {
+        id: 4,
+        title: 'Testing Vue App',
+        content: 'I am writing some tests for my Vue App',
+        likes: 12,
+        hashtags: ['test', 'vue']
+    },
+]
